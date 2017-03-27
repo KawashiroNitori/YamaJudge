1c301d232693410834f0e51ce6077fa3fd0a044a
Nitori <nitori@ence-dimension.com>
2017-03-27 16:10:24 +0800

    upgrade python to 3

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ cd judger
 rm -rf build && mkdir build && cd build && cmake ..
 make || exit 1
 make install
-cd ../bindings/Python && rm -rf build && python setup.py install || exit 1
+cd ../bindings/Python && rm -rf build && python3 setup.py install || exit 1
